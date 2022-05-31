f8dec7feceb9f975b7c242b34c8fc3ec7a3f2c54
Massimiliano Picone <87522343+Pmax8@users.noreply.github.com>
2022-05-31 19:47:02 +0200

    Add files via upload

--- CryptoPedia 2.0/Local/Feedback/feedback.php
+++ CryptoPedia 2.0/Local/Feedback/feedback.php
@@ -21,9 +21,10 @@
         ?>
 
         <div class="alert alert-info alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><div aria-hidden="true">&times;</div></button>
+            <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close"><div aria-hidden="true">&times;</div></button>
             <?php 
-                echo "<h1 style="text-align:center;"><strong> Feedback sent! </strong></h1>"; 
+                echo "<h1 style='text-align:center;'><strong> Feedback sent! </strong></h1>"; 
+                
             ?>
         </div>
 
